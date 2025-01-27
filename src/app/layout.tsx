7b9db61880cd4d67165a57b522e0994c96104703
sasakarma <sakashun.ps@gmail.com>
2025-01-27 23:57:08 +0900

    explicitly assigned key for theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
+          storageKey="theme-layout"
         >
           {children}
         </ThemeProvider> 
